7c665e786ada47500fb4fc08c178267506870886
ljgarcia <leylajael@gmail.com>
2015-11-20 23:12:09 +0000

    Add cloud of tags.

---
+++ snippets/simple.js
@@ -0,0 +1,11 @@
+// if you don't specify a html file, the sniper will generate a div
+var appDiv = document.createElement('div');
+yourDiv.appendChild(appDiv);
+
+var app = require("biotea-vis-annotation");
+var instance = new app({
+    el: appDiv,
+    db: 'pmc',
+    path: 'http://localhost:9090/data/',
+    id: 1669719
+});
